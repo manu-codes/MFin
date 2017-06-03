4424f42638aaddf17f5c98cc3452d2950fefc183
manuanalakkad <manuanalakkad@gmail.com>
2017-06-03 14:06:09 +0530

    Dbchanges

--- app/dbserver.js
+++ app/dbserver.js
@@ -1,38 +1,39 @@
 const express = require('express')
 const low = require('lowdb')
 const fileAsync = require('lowdb/lib/storages/file-async')
-
+const DATA_PATH = 'data/';
+const DB_NAME = 'mfin';
 // Create server
 const app = express()
 
 // Start database using file-async storage
 // For ease of use, read is synchronous
-const db = low('db.json', {
+const db = low(DATA_PATH+DB_NAME+'.json', {
   storage: fileAsync
 })
 
 // Routes
-// GET /posts/:id
-app.get('/posts/:id', (req, res) => {
-  const post = db.get('posts')
+// GET /tests/:id
+app.get('/tests/:id', (req, res) => {
+  const test = db.get('tests')
     .find({ id: req.params.id })
     .value()
 
-  res.send(post)
+  res.send(test)
 })
 
-// POST /posts
-app.post('/posts', (req, res) => {
-  db.get('posts')
+// test /tests
+app.test('/tests', (req, res) => {
+  db.get('tests')
     .push(req.body)
     .last()
     .assign({ id: Date.now() })
     .write()
-    .then(post => res.send(post))
+    .then(test => res.send(test))
 })
 
 // Init
-db.defaults({ posts: [] })
+db.defaults({ tests: [] })
   .write()
   .then(() => {
     app.listen(3000, () => console.log('Server is listening')
